da2ce619cc15ad7f72d8a67a0cb67ee1e24c2811
Snugglez <32309975+Snugglez@users.noreply.github.com>
2019-10-19 07:57:41 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require("fs")
+const path = require("path")
+module.exports = function reee(d) {
+let vidPath = path.join(d.clientInterface.info.path, '\\..\\S1Game\\CookedPC\\Art_Data\\Packages\\Video\\')
+let files = fs.readdirSync(vidPath, {withFileTypes: true})
+function unname(){
+files = fs.readdirSync(vidPath, {withFileTypes: true})
+files.forEach(file => {if(file.name.includes('_DISABLED')){fs.rename(path.join(vidPath, file.name), path.join(vidPath, file.name.split('_DISABLED')[0]), (e) => {if (e) {}})}})
+}
+files.forEach(file => {if(file.name.includes('intro_') && !file.name.includes('_DISABLED')){fs.rename(path.join(vidPath, file.name), path.join(vidPath, file.name + '_DISABLED'), (e) => {if (e) {}})}})
+d.clientInterface.on('data', (e, data) => {if(e == 'get_sls'){unname()}
+})
+this.destructor = () => {unname()}
+}
